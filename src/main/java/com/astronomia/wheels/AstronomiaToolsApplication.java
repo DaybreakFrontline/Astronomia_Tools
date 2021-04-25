1c44d637610d14753dfa90d435fa729ef1095452
panjiufeng <185239322pan>
2021-04-25 19:45:47 +0800

    Initial commit

---
+++ src/main/java/com/astronomia/wheels/AstronomiaToolsApplication.java
@@ -0,0 +1,13 @@
+package com.astronomia.wheels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstronomiaToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstronomiaToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/astronomia/tools/AstronomiaToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astronomia.wheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstronomiaToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
